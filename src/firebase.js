801982f1ec155e76e18cdc6edbf9f30117449b09
sameer <sameerchikshe.1@gmail.com>
2023-01-06 14:38:18 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBd2Q0ZDUKFMf4PnxGLK6fWw4iG-oFXxmA",
-  authDomain: "movie-app-17ffa.firebaseapp.com",
-  projectId: "movie-app-17ffa",
-  storageBucket: "movie-app-17ffa.appspot.com",
-  messagingSenderId: "165569395641",
-  appId: "1:165569395641:web:56d3d533dcf925615e1468",
-  measurementId: "G-N74QH5MD38"
+  apiKey: "AIzaSyBYuKHbg21HJAT31fVUtnlBit1Etmd69l8",
+  authDomain: "movie-clone-d1ce9.firebaseapp.com",
+  projectId: "movie-clone-d1ce9",
+  storageBucket: "movie-clone-d1ce9.appspot.com",
+  messagingSenderId: "205617919368",
+  appId: "1:205617919368:web:2afbb94121483247d3763d",
+  measurementId: "G-V5K402VZNT"
   };
 
 // Use this to initialize the firebase App
